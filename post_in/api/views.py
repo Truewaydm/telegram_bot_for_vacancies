516894de9b912aeef7997beaca5424140d30bb77
Rainbowdm <dim8035@gmail.com>
2023-03-15 16:42:55 +0200

    005_006_the_function_of_detailing,_editing_and_deleting_a_specific_record

--- post_in/api/views.py
+++ post_in/api/views.py
@@ -21,3 +21,26 @@ def notes_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def notes_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
