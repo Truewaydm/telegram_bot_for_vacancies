342dde323e26c0f2adcb47f8a4ffc89c70e2458c
Rainbowdm <dim8035@gmail.com>
2023-03-24 17:02:19 +0200

    007_006 django filter for filtering and outputs

--- scraping_service/api/views.py
+++ scraping_service/api/views.py
@@ -1,20 +1,40 @@
 import datetime
-
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 from scraping.models import City, Vacancy, Language
 from .serializers import *
 
 period = datetime.date.today() - datetime.timedelta(1)
 
 
+class DateFilterBackend(filters.BaseFilterBackend):
+    """
+    Filter that only allows users to see their own objects.
+    https://www.django-rest-framework.org/api-guide/filtering/
+    """
+
+    def filter_queryset(self, request, queryset, view):
+        # return queryset.filter(timestamp__gte=period)
+        city_slug = request.query_params.get('city', None)
+        language_slug = request.query_params.get('language', None)
+        return queryset.filter(
+            city__slug=city_slug,
+            language__slug=language_slug,
+            timestamp__gte=period)
+
+
 class CityViewSet(ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class LanguageViewSet(ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class VacancyViewSet(ModelViewSet):
@@ -23,21 +43,24 @@ class VacancyViewSet(ModelViewSet):
     """
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    filter_backends = [DjangoFilterBackend, DateFilterBackend]
+    filterset_fields = ['city__slug', 'language__slug']
 
-    def get_queryset(self):
-        city_slug = self.request.query_params.get('city', None)
-        language_slug = self.request.query_params.get('language', None)
-        qs = None
-        if city_slug and language_slug:
-            qs = Vacancy.objects.filter(
-                city__slug=city_slug,
-                language__slug=language_slug, timestamp__gte=period)
-            if not qs.exists():
-                qs = Vacancy.objects.filter(
-                    city__slug=language_slug,
-                    language__slug=city_slug, timestamp__gte=period)
-        self.queryset = qs
-        return self.queryset
+    # def get_queryset(self):
+    #     city_slug = self.request.query_params.get('city', None)
+    #     language_slug = self.request.query_params.get('language', None)
+    #     qs = None
+    #     if city_slug and language_slug:
+    #         qs = Vacancy.objects.filter(
+    #             city__slug=city_slug,
+    #             language__slug=language_slug, timestamp__gte=period)
+    #         if not qs.exists():
+    #             qs = Vacancy.objects.filter(
+    #                 city__slug=language_slug,
+    #                 language__slug=city_slug, timestamp__gte=period)
+    #     self.queryset = qs
+    #     return self.queryset
 
     # def get_queryset(self):
     #     city_slug = self.request.query_params.get('city', None)
