64e21993c8b20faf6cda838fd2b25b9c068d0440
Rainbowdm <dim8035@gmail.com>
2023-03-28 12:36:17 +0300

    008_005 View based on the MethodView class

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.views import MethodView
 from flask import request
 
 app = Flask(__name__)
@@ -12,6 +13,19 @@ def index():
         return '<h1> Hi Telegram!!! </h1>'
     return '<h1> Hi BOT!!! </h1>'
 
+
+class BotApi(MethodView):
+
+    def get(self):
+        return '<h1> Hi BOT_Class!!! </h1>'
+
+    def post(self):
+        response = request.get_json()
+        print(response)
+        return '<h1> Hi Telegram_Class!!! </h1>'
+
+
+app.add_url_rule('/TOKEN/', view_func=BotApi.as_view('bot'))
 
 if __name__ == '__main__':
     app.run()
